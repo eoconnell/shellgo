e8f0fbbb025e4ae1a6f25c64dc532d8e51448ae2
Evan O'Connell <oconn.e@gmail.com>
2019-09-15 22:33:35 -0700

    Rewrite tests as examples

--- bash_test.go
+++ bash_test.go
@@ -1,77 +1,49 @@
 package main
 
-import "testing"
-
 import (
-  "bytes"
-  "bufio"
+  "os"
 )
 
-func TestGenerateRaw(t *testing.T) {
-  var buf bytes.Buffer
-  out := bufio.NewWriter(&buf)
+func ExampleGenerate_Raw() {
   sh := NewShell()
   sh.raw("exit 1")
 
-  Generate(sh, out)
+  Generate(sh, os.Stdout)
 
-  out.Flush()
-  expected := "exit 1\n"
-  actual := buf.String()
-  if actual != expected {
-    t.Errorf("Expected `%s` but got `%s`", expected, actual)
-  }
+  // Output:
+  // exit 1
 }
 
-func TestGenerateCmd(t *testing.T) {
-  var buf bytes.Buffer
-  out := bufio.NewWriter(&buf)
+func ExampleGenerate_Cmd() {
   sh := NewShell()
   sh.cmd("python --version")
 
-  Generate(sh, out)
+  Generate(sh, os.Stdout)
 
-  out.Flush()
-  expected := "travis_cmd python\\ --version\n"
-  actual := buf.String()
-  if actual != expected {
-    t.Errorf("Expected `%s` but got `%s`", expected, actual)
-  }
+  // Output:
+  // travis_cmd python\ --version
 }
 
-func TestGenerateExport(t *testing.T) {
-  var buf bytes.Buffer
-  out := bufio.NewWriter(&buf)
+func ExampleGenerate_Export() {
   sh := NewShell()
   sh.export("KEY", "value")
 
-  Generate(sh, out)
+  Generate(sh, os.Stdout)
 
-  out.Flush()
-  expected := "export KEY=value\n"
-  actual := buf.String()
-  if actual != expected {
-    t.Errorf("Expected `%s` but got `%s`", expected, actual)
-  }
+  // Output:
+  // export KEY=value
 }
 
-func TestGenerateIf(t *testing.T) {
-  var buf bytes.Buffer
-  out := bufio.NewWriter(&buf)
+func ExampleGenerate_If() {
   sh := NewShell()
   sh.if_("-e requirements.txt", func() {
     sh.cmd("pip install -r requirements.txt")
   })
 
-  Generate(sh, out)
+  Generate(sh, os.Stdout)
 
-  out.Flush()
-  expected := `if [[ -e requirements.txt ]]; then
-    travis_cmd pip\ install\ -r\ requirements.txt
-fi
-`
-  actual := buf.String()
-  if actual != expected {
-    t.Errorf("Expected `%s` but got `%s`", expected, actual)
-  }
+  // Output:
+  // if [[ -e requirements.txt ]]; then
+  //     travis_cmd pip\ install\ -r\ requirements.txt
+  // fi
 }
